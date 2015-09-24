ce1654cdf3125e72449f50b04297c728fa70a4c8
Alexander Gama Espinosa <alexgama02@hotmail.com>
2015-09-24 18:27:54 -0500

    Graficas finalizadas

--- php/accidentes.php
+++ php/accidentes.php
@@ -22,6 +22,36 @@ function nombreTipo($category) {
   return 'Hay un error';
 }
 
+function nombreCausa($category) {
+  switch ($category) {
+    case 'CV':
+      return 'Colisión con vehículo automotor';
+    case 'CO':
+      return 'Colisión con objeto fijo';
+    case 'CC':
+      return 'Colisión con ciclista';
+    case 'CF':
+      return 'Colisión con ferrocarril';
+    case 'CN':
+      return 'Colisión con animal';
+    case 'V':
+      return 'Volcadura';
+    case 'CP':
+      return 'Caída de pasajero';
+    case 'CA':
+      return 'Colisión con peatón (atropellamiento)';
+    case 'SC':
+      return 'Salida del camino';
+    case 'IN':
+      return 'Incendio';
+    case 'NF':
+      return 'Colisión con motocicleta';
+    case 'OT':
+      return 'Otro';
+  }
+  return 'Hay un error';
+}
+
 header('content-type: application/json; charset=utf-8');
 header("access-control-allow-origin: *");
 
@@ -32,32 +62,93 @@ if(mysqli_connect_errno()) {
 
 if(!empty($_POST['mes']) && !empty($_POST['causa']) && !empty($_POST['sexo']) 
    && !empty($_POST['alcohol'])  && !empty($_POST['cinturon']) && isset($_POST['consulta'])) {
-      
-  $SQL = "SELECT `tipo`, `accidentes` FROM `accidentes` WHERE `mes` = ? AND `causa` = ?".
-          " AND `sexo` = ? AND `alcohol` = ? AND `cinturon` = ?";
-  if (!($stmt = $mainDB->prepare($SQL))) {
-    finish(htmlspecialchars($mainDB->error), $mainDB);
+  switch($_POST['consulta']) {
+    case 1:
+        $SQL = "SELECT `tipo`, `accidentes` FROM `accidentes` WHERE `mes` = ? AND `causa` = ?".
+         " AND `sexo` = ? AND `alcohol` = ? AND `cinturon` = ?";
+      break;
+    case 2:
+      $SQL = "SELECT `tipo`, `accidentes` FROM `accidentes` WHERE `mes` = ? AND `causa` = ?".
+         " AND `sexo` = ? AND `alcohol` = ? AND `cinturon` = ? AND `tipo` != 'T'";
+      break;
+    case 3:
+      $SQL = "SELECT `mes`, `accidentes` FROM `accidentes` WHERE `causa` = ?".
+         " AND `sexo` = ?  AND `alcohol` = ? AND `cinturon` = ? AND `tipo` = 'T'";
+      break;
+    case 4:
+      $SQL = "SELECT `sexo`, `tipo`, `accidentes`, `causa` FROM `accidentes` WHERE ".
+          " `mes` = ?  AND `alcohol` = ? AND `cinturon` = ? AND `tipo` = 'T' AND `causa` != 'T'";
+      break;
   }
-  
-  $stmt->bind_param("sssss", $_POST['mes'], $_POST['causa'], $_POST['sexo'], $_POST['alcohol'], 
-                    $_POST['cinturon']);
-  if (!$stmt->execute()) {
+  if (!($stmt = $mainDB->prepare($SQL))) {
     finish(htmlspecialchars($mainDB->error), $mainDB);
   }
   
   $keys  = array();
   $result_raw = array();
-  $stmt->bind_result($tipo, $accidentes);
-  while($stmt->fetch()) {
-    $keys[] = $tipo;
-    $result_raw[] = array(nombreTipo($tipo), $accidentes);
-  }
-  
-  $items = array('T', 'FA', 'NF', 'SD'); // Esto es para poner en ceros los datos que no encuentre
-  $result = array();
-  for($i = 0; $i < 4; $i++) {
-    $result[] = (($j = array_search($items[$i], $keys)) !== false)? $result_raw[$j] : 
-                                                                    array(nombreTipo($items[$i]),  0);
+  switch($_POST['consulta']) {
+    case 1:
+    case 2:
+      $stmt->bind_param("sssss", $_POST['mes'], $_POST['causa'], $_POST['sexo'], $_POST['alcohol'], 
+                    $_POST['cinturon']);
+      if (!$stmt->execute()) {
+      finish(htmlspecialchars($mainDB->error), $mainDB);
+      }
+    
+      $stmt->bind_result($tipo, $accidentes);
+      while($stmt->fetch()) {
+      $keys[] = $tipo;
+      $result_raw[] = array(nombreTipo($tipo), $accidentes);
+      }
+      
+      $items = array('T', 'FA', 'NF', 'SD'); // Esto es para poner en ceros los datos que no encuentre
+      $result = array();
+      for($i = 0; $i < 4; $i++) {
+      $result[] = (($j = array_search($items[$i], $keys)) !== false)? $result_raw[$j] : 
+                                      array(nombreTipo($items[$i]),  0);
+      }
+      break;
+    case 3:
+      $stmt->bind_param("ssss", $_POST['causa'], $_POST['sexo'], $_POST['alcohol'], 
+                    $_POST['cinturon']);
+      if (!$stmt->execute()) {
+      finish(htmlspecialchars($mainDB->error), $mainDB);
+      }
+    
+      $stmt->bind_result($mes, $accidentes);
+      while($stmt->fetch()) {
+      $keys[] = $mes;
+      $result_raw[] = array($mes, $accidentes);
+      }
+      
+      $items = array('EN', 'FE', 'MA', 'AB', 'MY', 'JU', 'JL', 'AG', 'SE', 'OC', 'NO', 'DI');
+      $result = array();
+      for($i = 0; $i < 12; $i++) {
+      $result[] = (($j = array_search($items[$i], $keys)) !== false)? $result_raw[$j] : 
+                                      array($items[$i],  0);
+      }
+      break;
+    case 4:
+        $stmt->bind_param("sss", $_POST['mes'], $_POST['alcohol'], $_POST['cinturon']);
+      if (!$stmt->execute()) {
+      finish(htmlspecialchars($mainDB->error), $mainDB);
+      }
+      
+      $stmt->bind_result($category, $tipo, $accidentes, $causa);
+      $gender = array('SF', 'M', 'H', 'T');
+      while($stmt->fetch()) {
+      $keys[] = $tipo;
+      $result_raw[] = array(' ', array_search($category, $gender) + 1, $accidentes, nombreCausa($causa), $accidentes);
+      }
+      
+      /*$items = array( 'FA', 'NF', 'SD'); // Esto es para poner en ceros los datos que no encuentre
+      $result = array();
+      for($i = 0; $i < 3; $i++) {
+      $result[] = (($j = array_search($items[$i], $keys)) !== false)? $result_raw[$j] : 
+                                      array(nombreTipo($items[$i]), 0, 0);
+      }*/
+      $result = $result_raw;
+      break;
   }
   
   print json_encode($result);
